3f2f687b6e4ffbec3a2e119d401c05768c2f1143
Afaque Wasif <124312248+wasifafaque8@users.noreply.github.com>
2026-04-23 12:37:43 +0530

    update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch43 team with  Mohit, Yogesh , Sachoin");
+     System.out.println("Hello batch43 team with  Mohit, Wasif , Sachoin");
       System.out.println("Hello Batch batch42 WITH FOR STUDENTS RUNNING in front of me and Anurag Prince in making CI automated vikas teach devops CI proecss");
     }  
 } 
